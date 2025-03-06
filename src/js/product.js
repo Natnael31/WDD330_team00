41a0d35fe05360e834629870eadf8409b3c37262
sofiapantas <sofiasarabia0831@gmail.com>
2025-03-07 00:35:58 +0800

    solution 2

--- src/js/product.js
+++ src/js/product.js
@@ -3,11 +3,19 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// function addProductToCart(product) {
+//   const items = getLocalStorage("so-cart")|| [];
+//   items.push(product)
+//   setLocalStorage("so-cart", items);
+// }
+
 function addProductToCart(product) {
-  const items = getLocalStorage("so-cart")|| [];
-  items.push(product)
-  setLocalStorage("so-cart", items);
+  let cart = localStorage.getItem("so-cart");
+  cart = cart ? JSON.parse(cart) : [];
+  cart.push(product);
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
