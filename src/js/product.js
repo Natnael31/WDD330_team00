dff7f93b4a73016c33e062e68b6c9c51fa364685
sofiapantas <sofiasarabia0831@gmail.com>
2025-03-07 00:38:11 +0800

    solution

--- src/js/product.js
+++ src/js/product.js
@@ -3,17 +3,10 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-// function addProductToCart(product) {
-//   const items = getLocalStorage("so-cart")|| [];
-//   items.push(product)
-//   setLocalStorage("so-cart", items);
-// }
-
 function addProductToCart(product) {
-  let cart = localStorage.getItem("so-cart");
-  cart = cart ? JSON.parse(cart) : [];
-  cart.push(product);
-  localStorage.setItem("so-cart", JSON.stringify(cart));
+  const items = getLocalStorage("so-cart")|| [];
+  items.push(product)
+  setLocalStorage("so-cart", items);
 }
 
 // add to cart button event handler
