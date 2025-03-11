f0c1da750de3c50b4a0eea16ed12920401673e9a
Natnael31 <end24002byui.edu>
2025-03-11 15:24:16 +0300

    W02 Team Activity: Dynamic Product Detail Page with corrected addToCart() method in ProductDetails.mjs

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+=======
+import { getParam } from "./utils.mjs";
+>>>>>>> a96f5cd (W02 Team Activity: Dynamic Product Detail Page with corrected addToCart() method in ProductDetails.mjs)
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+console.log(productId);
+console.log(dataSource.findProductById(productId));
 
+<<<<<<< HEAD
 function addProductToCart(product) {
   let array = getLocalStorage("so-cart") || [];
   array.push(product);
@@ -19,3 +28,7 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+=======
+const product = new ProductDetails(productId, dataSource);
+product.init();
+>>>>>>> a96f5cd (W02 Team Activity: Dynamic Product Detail Page with corrected addToCart() method in ProductDetails.mjs)
