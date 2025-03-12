bff74dcedb17e8974f1152345a928aecf272a850
kcccendana <kc_telyn@yahoo.com>
2025-03-13 00:07:32 +0900

    Create instance of ProductList

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs"
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
-const productList = new ProductList("tents", dataSource, document.querySelector("#product-list"));
-productList.init();
+// const productData = new ProductData("tents");
+const element = document.querySelector(".product-list")
+const dataSource = new ProductData("tents")
+const tentProductList = new ProductList("tents", dataSource, element)
+
+tentProductList.init()
