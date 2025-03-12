e7b5008b93d2c6da87abd552bed62701f2f29b3e
sofiapantas <sofiasarabia0831@gmail.com>
2025-03-12 19:54:01 +0800

    product list

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import ProductList from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list")
+const element = document.querySelector(".product-list");
 const list = new ProductList("Tents", dataSource, element);
 list.init();
