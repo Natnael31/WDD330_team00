7a870c605c711c661eee645212e593f5838f6f4d
sofiapantas <sofiasarabia0831@gmail.com>
2025-03-12 22:26:04 +0800

    week 2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -6,7 +6,7 @@ function productCardTemplate(product) {
         <a href="product_pages/index.html?product=${product.Id}">
             <img 
             src="${product.Image}" 
-            alt="Image of ${product.Brand.Name}" 
+            alt="Image of ${product.Name}" 
             />
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.Name}</h2>
